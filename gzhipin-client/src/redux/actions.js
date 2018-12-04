7ab74851ae7ff18ed74c7eb2212efafba8f27e3e
wangxiang <909980020@qq.com>
2018-12-04 14:53:40 +0800

    ajax请求发送以及redux组件完成

--- gzhipin-client/src/redux/actions.js
+++ gzhipin-client/src/redux/actions.js
@@ -1,3 +1,34 @@
 /**
  * Created by 王翔 on 2018-12-3.
  */
+/*
+ 作用：包含多个用来创建action的action creators
+ 类别：
+ 1. 同步action creator
+ 返回值是action对象
+ 2. 异步action creator
+ 返回值是函数 dispatch => {xxx}
+ */
+import {reqRegister} from '../api';
+import {AUTH_SUCCESS, AUTH_ERROR} from './action-types';
+//定义同步action creator
+export const authSuccess = data => ({type: AUTH_SUCCESS, data});
+export const authError = data => ({type: AUTH_ERROR, data});
+
+//定义异步action creator
+export const register = ({username, password, rePassword, type}) => {
+  return dispatch => {
+    //做异步任务，发送ajax请求
+    reqRegister({username, password, type})
+      .then(({data}) =>{
+      if (data.code === 0){
+        dispatch(authSuccess(data.data));
+      }else{
+        dispatch(authError({errMsg :data.msg}))
+      }
+      })
+      .catch(err =>{
+        dispatch(authError({errMsg: '网络不好，请刷新试试~'}))
+      })
+  }
+}
--- gzhipin-client/src/redux/reducers.js
+++ gzhipin-client/src/redux/reducers.js
@@ -1,24 +1,39 @@
-/**
- * Created by 王翔 on 2018-12-3.
+/*
+ 作用：根据之前的状态（previousState）和更新数据的行为（action）产生一个新的状态（newState）
  */
-import {combineReducers} from 'redux'
+import {combineReducers} from 'redux';
 
+import {AUTH_SUCCESS, AUTH_ERROR} from './action-types';
 
-const initXxxState  = 0
-function xxx(previousState = initXxxState,action) {
-  switch (action.type){
-    default:
-      return previousState
+//初始化状态的值
+const initUserState = {
+  username: '',
+  type: '',
+  _id: '',
+  errMsg: ''
+};
+
+function user(previousState = initUserState, action) {
+  switch (action.type) {
+    case AUTH_SUCCESS :
+      return action.data;
+    case AUTH_ERROR :
+      return {...initUserState, ...action.data};
+    default :
+      return previousState;
   }
 }
-const initYyyState  = 0
-function yyy(previousState = initYyyState,action) {
-  switch (action.type){
-    default:
-      return previousState
+
+const initYyyState = {};
+function yyy(previousState = initYyyState, action) {
+  switch (action.type) {
+    default :
+      return previousState;
   }
 }
+
+//默认暴露合并后的reducers函数
+// {xxx: function xxx() {}, yyy: function yyy() {}}
 export default combineReducers({
-  xxx,
-  yyy
+  user
 })
